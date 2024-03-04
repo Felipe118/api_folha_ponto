e0a3c622024a75ef9490e0b9cd6a2f9ef4bc3c23
Felipe118 <lipebezerraa@gmail.com>
2024-03-03 22:33:43 -0300

    ajustando os containers com php-fpm + nginx

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,20 +17,19 @@ return new class extends Migration
             $table->string('cpf')->unique();
             $table->string('rg')->unique();
             $table->string('phone')->unique();
-            //$table->string('address');
-            $table->boolval('active');
+            $table->boolean('active')->nullable()->comment('Ativo');
             $table->date('date_termination')->nullable()->comment('Data de desligamento');
-            $table->string('date_admission')->comment('Data de admissão');
-            $table->string('date_birth')->comment('Data de nascimento');
-            $table->string('user')->comment('usuario do sistema');
+            $table->string('date_admission')->comment('Data de admissão')->nullable();
+            $table->string('date_birth')->comment('Data de nascimento')->nullable();
+            $table->string('user')->comment('usuario do sistema')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
 
-
-            $table->foreignId('office_id');
-
+            // Foreign Key
+            $table->foreignId('office_id')->nullable();
             $table->foreign('office_id')->references('id')->on('office');
+            
             $table->rememberToken();
             $table->timestamps();
         });
